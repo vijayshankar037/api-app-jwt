ec137c21202acc660204c5e7b11b333e82448491
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-02-15 12:54:32 +0530

    items resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_065840) do
+ActiveRecord::Schema.define(version: 2020_02_15_072134) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
